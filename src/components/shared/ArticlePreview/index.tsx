fa12d5ff61d94df062488119edf3ae1854afdaba
bytesbanana <saharat.paynok@gmail.com>
2023-01-29 11:02:02 +0700

    feat(articles): prevent unauth toggle favorite

--- src/components/shared/ArticlePreview/index.tsx
+++ src/components/shared/ArticlePreview/index.tsx
@@ -6,35 +6,41 @@ import useSwr from "swr";
 import storage from "fetcher/storage";
 import { User } from "types/user";
 import { API_BASE_URL } from "utils/constant";
-import { ArticleResponse } from "types/response";
+import { useRouter } from "next/router";
 
 interface Props {
   article: Article;
 }
 
 export const ArticlePreview = ({ article }: Props) => {
+  const router = useRouter();
   const { data: currentUser } = useSwr<User>("user", storage);
-
-  const [favorited, setFavorited] = useState<boolean>(article.favorited)
-  const [favoritesCount, setFavoritesCount] = useState<number>(article.favoritesCount)
+  const [favorited, setFavorited] = useState<boolean>(article.favorited);
+  const [favoritesCount, setFavoritesCount] = useState<number>(
+    article.favoritesCount
+  );
 
   const toggleFavorite = async () => {
+    if (!currentUser) router.push("/login");
 
-    const response = await fetch(`${API_BASE_URL}/articles/${article.slug}/favorite`, {
-      method: favorited ? 'DELETE' : 'POST',
-      headers: {
-        'Authorization': 'Bearer ' + currentUser?.token
+    const response = await fetch(
+      `${API_BASE_URL}/articles/${article.slug}/favorite`,
+      {
+        method: favorited ? "DELETE" : "POST",
+        headers: {
+          Authorization: "Bearer " + currentUser?.token,
+        },
       }
-    })
+    );
 
     if (response.ok) {
-      setFavorited((isFav) => !isFav)
+      setFavorited((isFav) => !isFav);
       setFavoritesCount((favCount) => {
-        if (favorited) return favCount - 1
-        return favCount + 1
-      })
+        if (favorited) return favCount - 1;
+        return favCount + 1;
+      });
     }
-  }
+  };
 
   return (
     <div className="article-preview" key={article.slug}>
@@ -50,14 +56,16 @@ export const ArticlePreview = ({ article }: Props) => {
             {dayjs(article.createdAt).format("MMMM D, YYYY")}
           </span>
         </div>
-        <button className={`btn btn-outline-primary btn-sm pull-xs-right ${favorited ? "active" : ""}`} onClick={toggleFavorite}>
+        <button
+          className={`btn btn-outline-primary btn-sm pull-xs-right ${
+            favorited ? "active" : ""
+          }`}
+          onClick={toggleFavorite}
+        >
           <i className="ion-heart"></i> {favoritesCount}
         </button>
       </div>
-      <Link
-        href={currentUser ? "/article/" + article.slug : "/login"}
-        className="preview-link"
-      >
+      <Link href={"/article/" + article.slug} className="preview-link">
         <h1>{article.title}</h1>
         <p>{article.description}</p>
         <span>Read more...</span>
